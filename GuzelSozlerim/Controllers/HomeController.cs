ad8f2d854c3eb5a60c33eb5b80c5d22859661cd4
Berdan Atılım Şen <berdan_-_00505@hotmail.com>
2021-04-21 14:39:51 +0300

    Add project files.

---
+++ GuzelSozlerim/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using GuzelSozlerim.Data;
+using GuzelSozlerim.Extensions;
+using GuzelSozlerim.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace GuzelSozlerim.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _db = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.GuzelSozler.Include(x => x.Begenenler).ToList());
+        }
+
+        [Authorize] // Sadece üyeler kullanabilir
+        [HttpPost]
+        public IActionResult BegeniDurumunuGuncelle(int id, bool begenildiMi)
+        {
+            try
+            {
+                string userId = User.GetUserId();
+                var begeni = new KullaniciSoz() { GuzelSozId = id, KullaniciId = userId };
+
+                if (begenildiMi)
+                {
+                    if (!_db.KullaniciSozler.Contains(begeni))
+                    {
+                        _db.KullaniciSozler.Add(begeni);
+                    }
+                }
+                else
+                {
+                    if (_db.KullaniciSozler.Contains(begeni))
+                    {
+                        _db.KullaniciSozler.Remove(begeni);
+                    }
+                }
+
+                _db.SaveChanges();
+                return new EmptyResult();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GuzelSozlerim/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuzelSozlerim.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<Kullanici>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // composite primary key oluşturduk.
+            builder.Entity<KullaniciSoz>().HasKey(x => new { x.GuzelSozId, x.KullaniciId });
+
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<GuzelSoz> GuzelSozler { get; set; }
+
+        public DbSet<KullaniciSoz> KullaniciSozler { get; set; }
+    }
+}
